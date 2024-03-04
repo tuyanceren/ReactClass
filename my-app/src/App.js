a6bfa91f60e0d32ff65c2a5a6729280638793dc0
U190709028 <tuyanceren@icloud.com>
2024-03-04 15:53:27 +0300

    first try in react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  /*const paragragh = document.createElement("p");
+  paragragh.textContent = "Hello World!";
+  document.getElementById("root").append(paragragh);
+  */
+  return (
+    <div>
+      <p>Hello World!</p> 
+    </div>
+  );
+}
+
+export default App;
